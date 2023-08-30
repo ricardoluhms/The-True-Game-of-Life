7143d4991d9d5d42210caf98465213af94ee5898
ricardoluhms <ricardoluhms@gmail.com>
2023-08-29 22:16:59 -0400

    added several elements

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ tuition = {'Base': 1000, 'Medium': 3000, 'High': 6000, 'Very High': 10000}
 spender_profile = {'Average': 0.9, 'Big Spender': 1.05, 'Small Spender': 0.75}
 
 class Person:
-    def __init__(self, gender):
+    def __init__(self, gender, age_range=None):
         self.gender = gender
         self.initial_age()
         self.spender_prof = np.random.choice(['Average', 'Big Spender', 'Small Spender'], p=[0.5, 0.3, 0.2])
@@ -19,8 +19,38 @@ class Person:
         self.married = False
         self.history = []
 
-    def initial_age(self):
-        self.age = np.random.randint(0, 95)
+    def initial_age(self, age_range=None):
+        if age_range == 'Baby':
+            self.age = np.random.randint(0, 2)
+        elif age_range == 'Child':
+            self.age = np.random.randint(2, 13)
+        elif age_range == 'Teenager':
+            self.age = np.random.randint(13, 18)
+        elif age_range == 'Young Adult':
+            self.age = np.random.randint(18, 25)
+        elif age_range == 'Adult':
+            self.age = np.random.randint(25, 65)
+        elif age_range == 'Elder':
+            self.age = np.random.randint(65, 96)
+        else:
+            ### raise error if age_range is not specified
+            raise ValueError('age_range is not specified')
+        self.age_range = age_range
+
+    def update_age_range(self):
+        if 0 <= self.age <= 1:
+            age_range = 'Baby'
+        elif 2 <= self.age <= 12:
+            age_range = 'Child'
+        elif 13 <= self.age <= 17:
+            age_range = 'Teenager'
+        elif 18 <= self.age <= 24:
+            age_range = 'Young Adult'
+        elif 25 <= self.age <= 64:
+            age_range = 'Adult'
+        else:
+            age_range = 'Elder'
+        self.age_range
 
     def student_and_career_path_check(self):
         if self.age < 18:
@@ -71,8 +101,11 @@ class Person:
         return earnings
 
     def initial_balance(self):
-        self.balance = self.childhood_savings() + self.part_time_job() + self.full_time_job_static()
-
+        if self.loan > 0:
+            self.balance = self.childhood_savings() + self.part_time_job() + self.full_time_job_static() - self.loan
+        else:        
+            self.balance = self.childhood_savings() + self.part_time_job() + self.full_time_job_static()
+    
     def initial_career(self):
         if 25 <= self.age < 35:
             self.career = np.random.choice(['Base', 'Medium', 'High'], p=[0.5, 0.4, 0.1])
@@ -96,8 +129,18 @@ class Person:
 
     def student_loan(self):
         if self.career == 'Student':
-            self.loan = tuition[self.future_career] * years_of_study[self.future_career]
-            self.loan_term = np.random.randint(5, 20)
+            total_tuition = tuition[self.future_career] * years_of_study[self.future_career]
+            # Amount to be paid by loan
+            loan_required = total_tuition - self.balance
+            
+            if loan_required <= 0:
+                self.balance -= total_tuition
+                self.loan = 0
+                self.loan_term = 0
+            else:
+                self.loan = loan_required
+                self.loan_term = np.random.randint(5, 20)
+                self.balance = 0  # all money used to pay part of the tuition
         else:
             self.loan = 0
             self.loan_term = 0
@@ -154,15 +197,14 @@ class Person:
                 self.marriage()
                 _, total_cost = self.calculate_marriage_cost()
                 self.balance -= total_cost
-   
-                
+
     def calculate_marriage_cost(self):
         # Average cost per person
         avg_cost_per_person = 34000 / 100  # assuming average 100 guests
         
         # Randomize number of guests, more concentrated on 50 to 100
         guests = np.random.normal(loc=75, scale=20).astype(int)
-        guests = np.clip(guests, 50, 300)
+        guests = np.clip(guests, 25, 300)
         
         # Calculate cost based on random additional percentage up to 40%
         additional_percentage = np.random.uniform(0, 0.4)
@@ -171,14 +213,13 @@ class Person:
         
         return guests, total_cost
 
-
     def marriage(self):
         if self.age >= 18 and self.married == False:
             if self.gender == "Male":
                 spouse_gender = 'Female' if np.random.random() < 0.75 else 'Male'
             else:
                 spouse_gender = 'Male' if np.random.random() < 0.75 else 'Female'
-            spouse = Person(spouse_gender)
+            spouse = Person(spouse_gender,age_range=self.age_range)
             spouse.married = True
             self.spouse = spouse
             self.married = True
@@ -186,6 +227,7 @@ class Person:
 
     def age_up(self):
         self.age += 1
+        self.update_age_range()
         if self.age >= 18:
             if self.career == 'Student':
                 years_of_grad = self.initial_student_years_of_grad()
@@ -196,8 +238,11 @@ class Person:
             self.promotion()
             self.work()
             self.spend()
-            self.marriage_chance()
-            
+        if self.married and np.random.rand() <= 0.05:
+            self.have_child()
+        elif self.married == False and self.age >= 18 and np.random.rand() <= 0.025:
+            self.have_child()
+  
         self.update_history()
 
     def update_history(self):
@@ -251,8 +296,3 @@ class Person:
         if np.random.random() < prob:
             child = Person(np.random.choice(['Male', 'Female']))
             self.children.append(child)
-
-    ...
-
-
-
