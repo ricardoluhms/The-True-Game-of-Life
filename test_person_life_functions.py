2cedada34d795d54490849b40ca641efe25fe9b8
ricardoluhms <38567188+ricardoluhms@users.noreply.github.com>
2023-10-12 19:38:05 -0400

    created person life test file

---
+++ test_person_life_functions.py
@@ -0,0 +1,7 @@
+#%%
+from warnings import simplefilter
+simplefilter(action='ignore', category=FutureWarning)
+from main import Person_Life
+
+#%%
+### Create a code generates one person and ages up one by one year and check if all the values are correct and life events are correct
