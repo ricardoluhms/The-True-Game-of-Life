73493e4c9bdabdf4c6c59aece1f52fbdec2eaacd
supragyabajpai <54230107+supragyabajpai@users.noreply.github.com>
2023-10-19 19:51:19 -0400

    some minor check points

--- test_person_life_functions.py
+++ test_person_life_functions.py
@@ -1,7 +1,24 @@
 #%%
+import pandas as pd
 from warnings import simplefilter
 simplefilter(action='ignore', category=FutureWarning)
 from main import Person_Life
 
+person = Person_Life("Male",None,None,current_year=2023, age_range="Baby")
+
 #%%
 ### Create a code generates one person and ages up one by one year and check if all the values are correct and life events are correct
+for _ in range(13):
+    person.age_up()
+
+#%%
+
+history_df = person.history_df
+
+print(history_df)
+
+excel_file_name = "single_person_history.xlsx"  
+history_df.to_excel(excel_file_name, index=False)  
+
+print(f"Data exported to {excel_file_name}")
+#%%
