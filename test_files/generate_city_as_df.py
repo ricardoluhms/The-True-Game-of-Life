f5403e1abdc5d278fe85e4adf5f2b898f0c15ff8
ricardoluhms <38567188+ricardoluhms@users.noreply.github.com>
2024-05-05 12:27:06 -0400

    Update generate_city_as_df.py

--- test_files/generate_city_as_df.py
+++ test_files/generate_city_as_df.py
@@ -15,7 +15,6 @@ if not os.path.exists("C:/Users/ricar/Documents/GitHub/TGoL/test_files/data/new_
 df3.to_csv("C:/Users/ricar/Documents/GitHub/TGoL/test_files/data/new_pipeline/complete_city_test.csv", index=False)
 
 #%%
-
 population_histogram(df3)
 
 #death_count_by_age(df3)
