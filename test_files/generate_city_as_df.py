ebe92e413b2b5adca9c978a73c1c6b37428c3ebb
ricardoluhms <38567188+ricardoluhms@users.noreply.github.com>
2024-05-11 21:40:26 -0400

    Update generate_city_as_df.py

--- test_files/generate_city_as_df.py
+++ test_files/generate_city_as_df.py
@@ -6,8 +6,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from modules.dataframe_mode import *
 
 
-#%%
-df3 = time_function(generate_complete_city, 100, "Young Adult", 40000, 1950)
+#%%40000
+df3 = time_function(generate_complete_city, 100, "Young Adult", 20000, 1950)
 #%% check if folder exists
 if not os.path.exists("C:/Users/ricar/Documents/GitHub/TGoL/test_files/data/new_pipeline"):
     os.makedirs("C:/Users/ricar/Documents/GitHub/TGoL/test_files/data/new_pipeline")
